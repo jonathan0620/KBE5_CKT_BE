44c8bd7fbc88a3780ab7edf12eb34ef7d3ba2cc7
jonathan <145638699+jonathan0620@users.noreply.github.com>
2025-06-27 14:16:28 +0900

    feat: RabbitConfig에 EXCHANGE_NAME·DB_QUEUE·VIEW_QUEUE 상수 및 빈 등록
    - gps.topic.exchange 토픽 익스체인지 상수 및 Bean 추가
    - gps.db.queue, gps.view.queue 큐 상수 및 Bean 등록

--- collector/src/main/java/kernel360/ckt/collector/config/RabbitConfig.java
+++ collector/src/main/java/kernel360/ckt/collector/config/RabbitConfig.java
@@ -1,8 +1,6 @@
 package kernel360.ckt.collector.config;
 
-import org.springframework.amqp.core.AmqpAdmin;
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.QueueBuilder;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.context.annotation.Bean;
@@ -10,11 +8,34 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitConfig {
-    public static final String GPS_QUEUE = "gps.queue";
+
+    public static final String EXCHANGE_NAME = "gps.topic.exchange";
+    public static final String DB_QUEUE = "gps.db.queue";
+    public static final String VIEW_QUEUE = "gps.view.queue";
 
     @Bean
-    public Queue gpsQueue() {
-        return new Queue(GPS_QUEUE, true, false, false);
+    public TopicExchange gpsTopicExchange() {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Queue dbQueue() {
+        return new Queue(DB_QUEUE, true);
+    }
+
+    @Bean
+    public Queue viewQueue() {
+        return new Queue(VIEW_QUEUE, true);
+    }
+
+    @Bean
+    public Binding dbBinding() {
+        return BindingBuilder.bind(dbQueue()).to(gpsTopicExchange()).with("gps.db");
+    }
+
+    @Bean
+    public Binding viewBinding() {
+        return BindingBuilder.bind(viewQueue()).to(gpsTopicExchange()).with("gps.view");
     }
 
     @Bean
